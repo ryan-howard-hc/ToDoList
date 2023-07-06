090b499d2edaef69a79046536a4408c59c5bf105
Ryan Howard <133996155+ryan-howard-hc@users.noreply.github.com>
2023-07-06 15:38:26 -0400

    design

--- app/src/App.js
+++ app/src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import Header from './Header';
 import ToDoList from './ToDoList';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
     return (
+        <div className="body">
         <div className ="container">
         <>
         <ToDoList />
         </>
+        </div>
         </div>
     );
 }
