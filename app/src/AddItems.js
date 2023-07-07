6794b4ec3b1724ecffe4fbad581febc2a0591695
Ryan Howard <133996155+ryan-howard-hc@users.noreply.github.com>
2023-07-07 08:10:32 -0400

    Update AddItems.js

--- app/src/AddItems.js
+++ app/src/AddItems.js
@@ -3,16 +3,22 @@ import './AddItems.css';
 
 function AddItems({addItem}) {
     const [inputValue, setInputValue] = useState('');
+    const [dueDate, setDueDate] = useState('');
+
 
     function toDoSubmit(event) { 
       event.preventDefault(); //now i can read the error at least...
-        addItem(inputValue);
-        setInputValue('');
+      addItem(JSON.stringify({ inputValue, dueDate }));
+      setInputValue('');
+        setDueDate('');
     }
 
     function inputSubmit(event) {
         setInputValue(event.target.value)       //retrieves current input for the function, seems convoluted
     }
+      function dueDateSubmit(event) {
+    setDueDate(event.target.value);
+  }
 
     return (
       <div className='additems'>
@@ -22,6 +28,12 @@ function AddItems({addItem}) {
             value={inputValue}
             onChange={inputSubmit}
             placeholder="What else to do?"
+          />
+          <input
+          type="date"
+          value={dueDate}
+          onChange={dueDateSubmit}
+          placeholder="Due date"
           />
           <button className="btn btn-primary btn-lg fw-bold" style={{ fontWeight: 'bold', fontStyle: 'italic' }} type="submit">Add Item</button>
         </form>
